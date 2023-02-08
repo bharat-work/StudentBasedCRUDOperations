0594dd2782852c1fbe80820cd9ed10d204750ab4
bharat-work <bharatrwork2232@gmail.com>
2023-02-08 10:54:06 +0530

    Uploading Project

--- src/com/techpalle/MyProgram.java
+++ src/com/techpalle/MyProgram.java
@@ -1,27 +1,10 @@
 package com.techpalle;
 
-import java.sql.SQLException;
-
 public class MyProgram {
 
 	public static void main(String[] args) 
 	{
-		A a = new A();
 		
-		// a.creating();
-		
-		try 
-		{
-			a.deleting(3);
-		}
-		catch (ClassNotFoundException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
 		
 	}
 
